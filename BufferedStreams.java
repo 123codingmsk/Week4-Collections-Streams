ec050bad32cde887668cf436d2dfa84a14965bc5
Manoj <manojsaikumarakp@gmail.com>
2025-04-29 15:00:34 +0530

    bufferedstream updated

--- BufferedStreams.java
+++ BufferedStreams.java
@@ -2,6 +2,17 @@ import java.io.*;
 
 public class BufferedStreams{
 	public static void main(String[] a){
-		
+		try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream("names.txt"));
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("output.txt"))){
+			
+			byte[] arr = new byte[1024];
+			int bytesRead;
+			while((bytesRead = bis.read(arr)) != -1){
+				bos.write(arr, 0, bytesRead);
+			}
+			System.out.println("File copied into output");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
